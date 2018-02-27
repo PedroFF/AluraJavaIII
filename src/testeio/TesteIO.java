ac3bced887033c9b0b378d15fe1412790d157db3
pedroFreitas <soufreitas.pedro@gmail.com>
2018-02-27 11:17:25 -0300

    Aula04

--- src/testeio/TesteIO.java
+++ src/testeio/TesteIO.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package testeio;
 
-/**
- *
- * @author pedrofreitas
- */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+
 public class TesteIO {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        InputStream is = new FileInputStream("leitura.txt");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        OutputStream os = new FileOutputStream("saida.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+        
+        String linha = br.readLine();
+        while (linha!=null){
+        bw.append(linha);
+        bw.newLine();
+        linha = br.readLine();
+        }
+        
+        br.close();
+        bw.close();
     }
+    
     
 }
